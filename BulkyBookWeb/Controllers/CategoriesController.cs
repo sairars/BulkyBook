13a6879a1edc892fa93fc13a7cff44d4c0ac14a2
Saira Rashid <saira.rashid1216@gmail.com>
2023-04-03 09:21:52 -0400

    build page to view book categories

---
+++ BulkyBookWeb/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+    }
+}
