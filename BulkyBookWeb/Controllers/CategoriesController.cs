8509524c2ddd2457d0982f99102f6b4909baac13
Saira Rashid <saira.rashid1216@gmail.com>
2023-04-03 09:32:00 -0400

    Build feature to create new book categories

--- BulkyBookWeb/Controllers/CategoriesController.cs
+++ BulkyBookWeb/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -18,5 +19,18 @@ namespace BulkyBookWeb.Controllers
             return View(categories);
         }
 
+        public IActionResult Create()
+        {
+            return View("CategoryForm");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
